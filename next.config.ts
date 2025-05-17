feb6a0ae87ec7d24366289c5f1eeed540d184cf6
Rahul-at <rahul.lakhvara@avestatechnologies.com>
2025-05-18 02:07:51 +0530

    feat: created env file in next.js config
    Signed-off-by: Rahul-at <rahul.lakhvara@avestatechnologies.com>

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 };
 
 export default nextConfig;

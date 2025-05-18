d2be6ee4210335d74949e7fa01fefa10194a81af
Rahul-at <rahul.lakhvara@avestatechnologies.com>
2025-05-18 14:54:44 +0530

    fix: binding with kudos card
    Signed-off-by: Rahul-at <rahul.lakhvara@avestatechnologies.com>

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -15,15 +15,6 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ children, title = 'Authenticati
       </Head>
 
       <div className='min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
-        <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-          <div className='flex justify-center'>
-            <div className='w-32 h-32 relative'>
-              <Image src='/logo.svg' alt='Voyage Logo' layout='fill' objectFit='contain' priority />
-            </div>
-          </div>
-          <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>Voyage</h2>
-        </div>
-
         <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
           <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>{children}</div>
         </div>
